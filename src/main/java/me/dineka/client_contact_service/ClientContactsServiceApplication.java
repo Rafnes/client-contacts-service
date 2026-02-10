3a627538caa037bcc3cf88ad4a9bbd925fa5ac31
Rafnes <rafzograf@gmail.com>
2026-02-10 19:28:17 +0300

    initial commit

---
+++ src/main/java/me/dineka/client_contact_service/ClientContactsServiceApplication.java
@@ -0,0 +1,13 @@
+package me.dineka.client_contact_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientContactsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientContactsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dineka/client_contact_service/ClientContactsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dineka.client_contact_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientContactsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
